00ab3e09876d32c2a5aa49edf3ad122e2bc7ef01
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-04-27 13:56:46 +0200

    article-commented out

--- darek-project/src/Article.js
+++ darek-project/src/Article.js
@@ -3,18 +3,18 @@ import React, { useEffect } from "react";
 function Article(props) {
     const [articles, setArticles] = React.useState([]);
 
-    useEffect(() => {
+  //  useEffect(() => {
         fetch(`https://api.spaceflightnewsapi.net/v3/articles/${props.id}`)
             .then(res => res.json())
             .then(article => setArticles([article]))
             .catch(err => console.log(`błąd ${err}` ))
-    }, [props.id]);
+  //  }, [props.id]);
 
     
-
+    // console.log(articles); 
     return (
         <div>
-            Id: {articles.imageUrl}
+           
         </div>
     );
 }
