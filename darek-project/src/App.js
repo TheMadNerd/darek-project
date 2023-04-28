6a1e90e27f8f2e6c1a50d4ddbd9a97b1b27b8661
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-04-28 16:18:43 +0200

    loop-problem

--- darek-project/src/App.js
+++ darek-project/src/App.js
@@ -22,20 +22,21 @@ useEffect(()=>{
   .then(articles => setArticle(articles.map(article => 
   <div className='box' key={article.id}>
     <div className='img'>
-    <img className='articleImg' src={article.imageUrl}></img>
+    <img className='articleImg' src={article.imageUrl} alt='imageApp'></img>
     </div>
     <div className='text'>
     <div className='title'>{article.title}</div> 
     <div className='content'>{article.summary}</div>
     </div>
     <div className='more' >
-      <NavLink to="article" id={article.id} onClick={navLinkHandler}>MORE</NavLink>
+      <NavLink to={`/article/${article.id}`} id={article.id} onClick={navLinkHandler}>MORE</NavLink>
     </div>
   </div>)
 
   ))
   .catch(err => console.log(`błąd ${err}` ))
-});
+}, [amount]);
+
 
   const buttonHandler = e => {
     setAmount(e.target.value)
@@ -48,10 +49,7 @@ useEffect(()=>{
   
   return (
     <div>
-    <Routes>
-      <Route path='/article' element={<Article id={idName}/>}></Route>
-      <Route path='/' element={App}></Route>
-    </Routes>
+    
     <div className='menu'>
       Articles amount:
       <div className='buttons'>
@@ -61,7 +59,10 @@ useEffect(()=>{
       </div>
     </div>
     <div className='boxes'>
-      {article}
+    <Routes>
+      <Route path={`/article/${idName}`} element={<Article id={idName}/>}></Route>
+      <Route path='/' element={article}></Route>
+    </Routes>
     </div>
     </div>
   );
--- darek-project/src/Article.js
+++ darek-project/src/Article.js
@@ -1,22 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+import './Article.css'
 
 function Article(props) {
-    const [articles, setArticles] = React.useState([]);
+	const [articles, setArticles] = React.useState([])
 
-  //  useEffect(() => {
-        fetch(`https://api.spaceflightnewsapi.net/v3/articles/${props.id}`)
-            .then(res => res.json())
-            .then(article => setArticles([article]))
-            .catch(err => console.log(`błąd ${err}` ))
-  //  }, [props.id]);
+	const url = window.location.href
+	const parts = url.split('/')
+	const lastPart = parts.pop()
+	console.log(lastPart)
 
-    
-    // console.log(articles); 
-    return (
-        <div>
-           
-        </div>
-    );
+	useEffect(() => {
+		fetch(`https://api.spaceflightnewsapi.net/v3/articles/${lastPart}`)
+			.then(res => res.json())
+			.then(article => setArticles(article))
+			.catch(err => console.log(`błąd ${err}`))
+	}, [])
+
+	const dateString = `${articles.publishedAt}`
+	const date = new Date(dateString)
+	const year = date.getFullYear()
+	const month = (date.getMonth() + 1).toString().padStart(2, '0')
+	const day = date.getDate().toString().padStart(2, '0')
+	const formattedDate = `${year}-${month}-${day}`
+
+	return (
+		<div className="article">
+			<div className="head">
+				<div className="articleTitle">{articles.title}</div>
+				<img className="photo" src={articles.imageUrl} alt="imageArticle"></img>
+				<div className="summary">{articles.summary}</div>
+
+				<div className="footer">
+					Published by: {articles.newsSite} at {formattedDate}
+					<div>
+						Link to original website: <Link to={articles.url}>{articles.newsSite}</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default Article;
+export default Article
