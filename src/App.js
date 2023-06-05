78add6f92fc19cafd26c2b85fcfad872e7102ae6
Bartosz Wysowski <bartoszwysowski@gmail.com>
2023-06-05 15:08:25 +0200

    single-article-update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Routes, Route, NavLink } from 'react-router-dom'
-import { parseISO, format } from 'date-fns';
+import { parseISO, format } from 'date-fns'
 import Article from './Article.js'
 import './App.css'
 
@@ -11,8 +11,6 @@ function App() {
 	const [amount, setAmount] = React.useState(10)
 	const [idName, setIdName] = React.useState(0)
 
-	
-
 	useEffect(() => {
 		fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${amount}`)
 			.then(res => res.json())
@@ -20,46 +18,58 @@ function App() {
 			.then(articles =>
 				setArticle(
 					articles.map(article => (
-
-
-
 						<div className="col-lg-3 flex-column d-flex align-items-stretch">
-
-							<NavLink className="lift lift-lg shadow-light-lg card h-100 card-spacer-y: 5px" key={article.id}  to={`/article/${article.id}`} onClick={() => navLinkHandler(article.id)}  style={{ textDecoration: 'none', width: '18rem'}}>
-
-								<div className='img-top'>
-								<img
-									src={article.imageUrl}
-									className="card-img-top"
-									alt="imageApp"
-									style={{ width: '100%', height: '15vw', objectFit: 'cover', }}></img>
+							<NavLink
+								className="lift lift-lg shadow-light-lg card h-100 card-spacer-y: 5px"
+								key={article.id}
+								to={`/article/${article.id}`}
+								onClick={() => navLinkHandler(article.id)}
+								style={{ textDecoration: 'none', width: '18rem' }}>
+								<div className="img-top">
+									<img
+										src={article.imageUrl}
+										className="card-img-top"
+										alt="imageApp"
+										style={{ width: '100%', height: '15vw', objectFit: 'cover' }}></img>
 
 									<div className="position-relative">
-									<div className="shape shape-bottom shape-fluid-x text-white">
-									<svg viewBox="0 0 2100 480" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z" fill="currentColor"></path></svg>                  </div>
+										<div className="shape shape-bottom shape-fluid-x text-white">
+											<svg viewBox="0 0 2100 480" fill="none" xmlns="http://www.w3.org/2000/svg">
+												<path
+													fillRule="evenodd"
+													clipRule="evenodd"
+													d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z"
+													fill="currentColor"></path>
+											</svg>{' '}
+										</div>
 									</div>
-									
-									
 								</div>
-								
+
 								<div className="card-body d-flex flex-column">
 									<h5 className="card-title">{article.title}</h5>
 
-									<p className="card-text" style={{whiteSpace: 'nowrap',overflow: 'hidden',overflowWrap: 'break-word',textOverflow: 'ellipsis',width: '250px', color: '#869ab8'}}>{article.summary}</p>
-
-
+									<p
+										className="card-text"
+										style={{
+											whiteSpace: 'nowrap',
+											overflow: 'hidden',
+											overflowWrap: 'break-word',
+											textOverflow: 'ellipsis',
+											width: '250px',
+											color: '#869ab8',
+										}}>
+										{article.summary}
+									</p>
 								</div>
-								<div className='card-meta mt-auto'>
-									<hr className='card-meta-divider'></hr>
+								<div className="card-meta mt-auto">
+									<hr className="card-meta-divider"></hr>
 									<h6 className="text-uppercase text-muted me-2 mb-0"> {article.newsSite}</h6>
-									<p className="h6 text-uppercase text-muted mb-0 ms-auto">{format(parseISO(article.publishedAt), "dd MMMM")}</p>
+									<p className="h6 text-uppercase text-muted mb-0 ms-auto">
+										{format(parseISO(article.publishedAt), 'dd MMMM')}
+									</p>
 								</div>
-
-
-
-								</NavLink>
-							</div>
-					
+							</NavLink>
+						</div>
 					))
 				)
 			)
@@ -70,17 +80,16 @@ function App() {
 		setAmount(e.target.value)
 	}
 
-	const navLinkHandler = (key) => {
+	const navLinkHandler = key => {
 		setIdName(key)
 	}
-
 
 	const navBar = (
 		<nav
 			class=" my-navbar navbar-collapse text-center  justify-content-center bg-body-tertiary fixed-top"
 			style={{ padding: '10px' }}>
 			<form class="container-fluid justify-content-start ">
-				<button class="my-button btn btn-sm btn-outline-secondary" type="button" value="5" onClick={buttonHandler} >
+				<button class="my-button btn btn-sm btn-outline-secondary" type="button" value="5" onClick={buttonHandler}>
 					5
 				</button>
 				<button class="my-button btn btn-sm btn-outline-secondary" type="button" value="15" onClick={buttonHandler}>
@@ -100,8 +109,6 @@ function App() {
 			<form class="container-fluid justify-content-start "></form>
 		</nav>
 	)
-
-	
 
 	return (
 		<div className="container">
--- src/Article.js
+++ src/Article.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
-
-
+import { parseISO, format } from 'date-fns'
 
 function Article(props) {
 	const [articles, setArticles] = React.useState([])
 
-	console.log(props.id);
+	console.log(props.id)
 
 	useEffect(() => {
 		fetch(`https://api.spaceflightnewsapi.net/v3/articles/${props.id}`)
@@ -23,20 +22,29 @@ function Article(props) {
 	const formattedDate = `${year}-${month}-${day}`
 
 	return (
-		
-		
-<div className="card flex-column d-flex align-items-stretch">
-<img src={articles.imageUrl} className="card-img-top mx-auto img-fluid" alt="articleImage" style={{width: "100%", height: "30vw", objectFit: "cover"}}></img>
-<div className="card-body h-100">
-  <h5 className="card-title">{articles.title}</h5>
-  <p className="card-text">{articles.summary}</p>
-</div>
-<ul className="list-group list-group-flush">
-  <li className="list-group-item">Published by: <span style={{fontWeight: "bold"}}>{articles.newsSite}</span></li>
-  <li className="list-group-item">Date of publish: <span style={{fontWeight: "bold"}}>{formattedDate}</span></li>
-  <li className="list-group-item">Link to original website: <span style={{fontWeight: "bold"}}><Link to={articles.url}>{articles.newsSite}</Link></span></li>
-</ul>
-</div>
+		<div>
+			<h1 className="display-4 text-center">{articles.title}</h1>
+			<p className="lead mb-7 text-center text-muted">{articles.summary}</p>
+			<div className="row align-items-center py-5 border-top border-bottom">
+				<div className="col ms-n5">
+					<h6 className="text-uppercase mb-0">{articles.newsSite}</h6>
+					<div className="fs-sm text-muted">Published on: {formattedDate}</div>
+				</div>
+				<div className="col-auto">
+					<span className="h6 text-uppercase text-muted d-none d-md-inline me-4">
+						Original website: <Link to={articles.url}>{articles.newsSite}</Link>
+					</span>
+				</div>
+			</div>
+
+			<div className="row justify-content-center" style={{ marginTop: '30px' }}>
+				<div className="col-12 col-md-10 col-lg-9 col-xl-8">
+					<figure className="figure mb-7">
+						<img className="figure-img img-fluid rounded lift lift-lg" src={articles.imageUrl} alt="photo"></img>
+					</figure>
+				</div>
+			</div>
+		</div>
 	)
 }
 
